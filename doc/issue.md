b4c282d3a028ac275c8c14f63a0a920e408bd767
jtong <tj19832@gmail.com>
2024-01-07 00:32:31 +0800

    make index.js can load crrocetly external

---
+++ doc/issue.md
@@ -0,0 +1,3 @@
+## issues
+
+- 如果传入的 contextPath 为空，则执行related_files函数时报错。要正确的抛出错误。
--- index.js
+++ index.js
@@ -5,5 +5,5 @@ module.exports = {
     folder_tree: require("./read_folder"),
     related_java_class_analysis: require("./related_java_class_analysis"),
     resolve_java_class_full_name: require("./resolve_java_class_full_name"),
-    prompt_render: require("prompt_render")
+    prompt_render: require("./prompt_render.js")
 }
